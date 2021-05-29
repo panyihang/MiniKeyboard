4c6ec63cc133917413fb88ac61d5efc8f7d74376
panyihang <admin@panyihang.top>
2021-05-29 14:21:42 +0800

    添加README

--- MiniKeyboard.ino
+++ MiniKeyboard.ino
@@ -8,9 +8,9 @@
 unsigned char buttonPinList[9] = {32, 14, 15, 16, 17, 31, 30, 11, 33};
 
 signed char littetList[32] = {0x2F, 0x2A, '-', '7', '8', '9', '+', '4',
-                                '5', '6', '1', '2', '3', 0xB0, '0', 0x2E,
-                                '/', '*', '-', 'q', 'w', 'e', 'f', 'a',
-                                's', 'd', '1', '2', '3', 0x20, '0',0x2E};
+                              '5', '6', '1', '2', '3', 0xB0, '0', 0x2E,
+                              'b', 'm', 0xB1, 'q', 'w', 'e', 'f', 'a',
+                              's', 'd', '1', '2', '3',0x20, 0x82, '4'};
 
 bool buttonFlags[17] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -21,52 +21,72 @@ char mainCount = 0;
 char offset = 0;
 bool gameMode = true;
 
-void setup() {
+void setup()
+{
   USBInit();
   pinMode(34, OUTPUT);
   digitalWrite(34, gameMode);
 }
 
-void loop() { defScanButton(); }
+void loop()
+{
+  defScanButton();
+}
 
-void defScanButton() {
+void defScanButton()
+{
   delay(50);
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++)
+  {
 
     pinMode(buttonPinList[i], OUTPUT);
     digitalWrite(buttonPinList[i], LOW);
 
-    for (int j = 5; j < 9; j++) {
+    for (int j = 5; j < 9; j++)
+    {
       pinMode(buttonPinList[j], INPUT_PULLUP);
       buttonTmp[mainCount] = !digitalRead(buttonPinList[j]);
-      if (buttonFlags[mainCount] != buttonTmp[mainCount]) {
+      if (buttonFlags[mainCount] != buttonTmp[mainCount])
+      {
         buttonFlags[mainCount] = buttonTmp[mainCount];
-        if (i == 0 && j == 5) {
+        if (i == 0 && j == 5)
+        {
           gameMode = !gameMode;
-          if (!gameMode) {
+          if (!gameMode)
+          {
             offset = 16;
-          } else {
+          }
+          else
+          {
             offset = 0;
           }
           digitalWrite(34, gameMode);
           delay(200);
-        } else {
-          if (buttonFlags[mainCount]) {
-            Keyboard_press(littetList[mainCount+offset]);
+        }
+        else
+        {
+          if (buttonFlags[mainCount])
+          {
+            Keyboard_press(littetList[mainCount + offset]);
             delay(20);
-          } else {
-            Keyboard_release(littetList[mainCount+offset]);
+          }
+          else
+          {
+            Keyboard_release(littetList[mainCount + offset]);
           }
         }
       }
       mainCount += 1;
-      if (i == 2 && j == 8) {
+      if (i == 2 && j == 8)
+      {
         mainCount -= 1;
       }
-      if (i == 4 && j == 6) {
+      if (i == 4 && j == 6)
+      {
         mainCount -= 1;
       }
-      if (i == 0 && j == 5) {
+      if (i == 0 && j == 5)
+      {
         mainCount -= 1;
       }
     }
@@ -74,3 +94,4 @@ void defScanButton() {
   }
   mainCount = 0;
 }
+
