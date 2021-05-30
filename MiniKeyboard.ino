6889dbcf3233ebc35873dc930f95b974c3a84dde
panyihang <admin@panyihang.top>
2021-05-30 12:25:46 +0800

    删除模式切换功能

--- MiniKeyboard.ino
+++ MiniKeyboard.ino
@@ -1,97 +1,56 @@
-
-#ifndef USER_USB_RAM
-#error "编译时开启USB以及USB用户代码空间"
-#endif
-
 #include "src/userUsbHidKeyboard/USBHIDKeyboard.h"
+#define gameMode false
+
+#if (gameMode)
+signed char littetList[20] = {'h', 'b',  'm',  0x1B, 'q',  'w', 'e',
+                              'f', 'a',  's',  'd',  0x20, '1', '2',
+                              '3', 0x20, 0x82, 0x20, '4'};
+#else
+signed char littetList[20] = {' ', '/',  '*',  '-', '7',  '8', '9',
+                              '+', '4',  '5',  '6',  0x20, '1', '2',
+                              '3', 0x0A, '0', 0x20, '.'};
+#endif
 
 unsigned char buttonPinList[9] = {32, 14, 15, 16, 17, 31, 30, 11, 33};
 
-signed char littetList[32] = {0x2F, 0x2A, '-', '7', '8', '9', '+', '4',
-                              '5', '6', '1', '2', '3', 0xB0, '0', 0x2E,
-                              'b', 'm', 0xB1, 'q', 'w', 'e', 'f', 'a',
-                              's', 'd', '1', '2', '3',0x20, 0x82, '4'};
-
-bool buttonFlags[17] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+bool buttonFlags[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-bool buttonTmp[17] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+bool buttonTmp[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-char mainCount = 0;
-char offset = 0;
-bool gameMode = true;
+unsigned char mainCount = 0;
 
-void setup()
-{
+void setup() {
   USBInit();
   pinMode(34, OUTPUT);
-  digitalWrite(34, gameMode);
+  digitalWrite(34, true);
 }
 
-void loop()
-{
-  defScanButton();
-}
+void loop() { defScanButton(); }
 
-void defScanButton()
-{
-  delay(50);
-  for (int i = 0; i < 5; i++)
-  {
+void defScanButton() {
+  delay(100);
+  for (unsigned char i = 0; i < 5; i++) {
 
     pinMode(buttonPinList[i], OUTPUT);
     digitalWrite(buttonPinList[i], LOW);
 
-    for (int j = 5; j < 9; j++)
-    {
+    for (unsigned char j = 5; j < 9; j++) {
+
       pinMode(buttonPinList[j], INPUT_PULLUP);
       buttonTmp[mainCount] = !digitalRead(buttonPinList[j]);
-      if (buttonFlags[mainCount] != buttonTmp[mainCount])
-      {
+      if (buttonFlags[mainCount] != buttonTmp[mainCount]) {
         buttonFlags[mainCount] = buttonTmp[mainCount];
-        if (i == 0 && j == 5)
-        {
-          gameMode = !gameMode;
-          if (!gameMode)
-          {
-            offset = 16;
-          }
-          else
-          {
-            offset = 0;
-          }
-          digitalWrite(34, gameMode);
-          delay(200);
-        }
-        else
-        {
-          if (buttonFlags[mainCount])
-          {
-            Keyboard_press(littetList[mainCount + offset]);
-            delay(20);
-          }
-          else
-          {
-            Keyboard_release(littetList[mainCount + offset]);
-          }
+        if (buttonFlags[mainCount]) {
+          Keyboard_press(littetList[mainCount]);
+          delay(20);
+        } else {
+          Keyboard_release(littetList[mainCount]);
         }
       }
       mainCount += 1;
-      if (i == 2 && j == 8)
-      {
-        mainCount -= 1;
-      }
-      if (i == 4 && j == 6)
-      {
-        mainCount -= 1;
-      }
-      if (i == 0 && j == 5)
-      {
-        mainCount -= 1;
-      }
     }
     digitalWrite(buttonPinList[i], HIGH);
   }
+
   mainCount = 0;
 }
-
